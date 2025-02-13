5a26f20a3bfe4bb88b7a8c9be7cd5577c31b7ded
Joshua Still <Joshua_Still@outlook.com>
2025-02-12 17:19:17 -0700

    finished game logic

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-console.log('test')
-
-let humanScore = 0;
-let computerScore = 0;
-
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -19,13 +14,6 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-
-    //let humanChoice = prompt('enter your choice: ROCK, PAPER, or SCISSORS').toLowerCase();
-
-    // if(humanChoice != 'rock' || 'paper' || 'scissors') {
-    //     console.log(`please enter ONLY: 'ROCK' , 'PAPER, OR SCISSORS`)
-    //     humanChoice = prompt('please enter ONLY enter ROCK, PAPER, or SCISSORS').toLowerCase();
-    // }
 
     let humanChoice;
 
@@ -57,21 +45,63 @@ function getHumanChoice() {
 
 
 
-
+//logic for 1 round
 function playRound(humanSelection,computerSelection) {
+    let result;
     if(humanSelection == computerSelection) {
         console.log('draw');
+        result = 'D';
     } else if (humanSelection == 0 && computerSelection == 2 || humanSelection == 1 && computerSelection == 0 || humanSelection == 2 && computerSelection == 1) {
         console.log('human wins this round')
+        result = 'H'
     } else if (humanSelection == 0 && computerSelection == 1 || humanSelection == 1 && computerSelection == 2 || humanSelection == 2 && computerSelection == 0) {
         console.log('computer wins this round')
+       result = 'C';
     }
+    console.log(result);
+    return result;
     
 }
 
-let humanSelection = getHumanChoice()
-let computerSelection = getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame() {
+
+    while (humanScore <= 4 && computerScore <= 4) {
+         // Get new selections for each round
+         let humanSelection = getHumanChoice();
+         let computerSelection = getComputerChoice();
+
+         // Play a round and get the result
+        let result = playRound(humanSelection, computerSelection);
+       
+    //count score
+    if(result == 'H') {
+        humanScore++;
+        console.log('human score is ' + humanScore);
+        alert('the winner of this round is the human: human score is : ' + humanScore);
+        playRound(humanSelection,computerSelection);
+    } else if (result == 'C'){
+        computerScore++
+        console.log('computer score is ' + computerScore);
+        alert('the winner of this round is the computer: computer score is : ' + computerScore);
+        playRound(humanSelection,computerSelection);
+    } else if (result == 'D'){
+        alert('DRAW no winner this round');
+        playRound(humanSelection,computerSelection);
+    }
+}
+
+//let user know who won
+if(humanScore > 4) {
+    alert('human wins the game')
+} else if (computerScore > 4) {
+    alert('computer wins the game')
+}
+ 
+}
 
 
-playRound(humanSelection,computerSelection);
-
+playGame();
